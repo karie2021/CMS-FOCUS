035bac89c4fa34a2ec0184dc5f5d348e781033a8
miinha <63690707+karie2021@users.noreply.github.com>
2021-10-26 13:47:05 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.single-item').slick({
+      dots: true,
+        accessibility: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      speed: 400
+    }); 
+    
+  });
